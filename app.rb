ac9a93c61be25a5cdf5902eec71f796b4c5fa0b9
Levchenko Oleg <zloyleva@gmail.com>
2016-05-08 21:33:35 +0300

    Add show datebase in web site

--- app.rb
+++ app.rb
@@ -5,7 +5,9 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def get_db
-	return SQLite3::Database.new 'barbershop.db'
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
 end
 
 configure do
@@ -60,3 +62,12 @@ post '/visit' do
 
 end
 
+get '/showusers' do
+	@users_table = ""
+	db = get_db
+	db.execute 'select * from Users' do |row|
+		@users_table += "<tr><td>#{row['id']}</td><td>#{row['username']}</td><td>#{row['phone']}</td><td>#{row['datestamp']}</td><td>#{row['barber']}</td><td>#{row['color']}</td></tr>\n"
+		#@users_table = @users_table + "<tr><td>" + row.to_s + "</tr></td>\n"
+	end
+	erb :showusers
+end
